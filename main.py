cacf2e11f9914ed3b9a16ac88ac4cef5ce99b31e
Shubhankargupta691 <shubhankargupta691@gmail.com>
2023-09-18 00:47:13 +0530

    updated: main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from Chicken_Disease_Classification import logger
+from Chicken_Disease_Classification.pipeline.pipeline import DataIngestionTrainingPipeline
 
-logger.info("welcome to my custom log")
+<<<<<<< HEAD
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f" >>>> Running stage: {STAGE_NAME} started ...")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> Stage Name: {STAGE_NAME} >>>> completed!\n\nx ========= x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+=======
+logger.info("Welconme to my custom log")
+>>>>>>> parent of 2987489 (Logger)
---
+++ src/Chicken_Disease_Classification/pipeline/pipeline.py
@@ -0,0 +1,26 @@
+from Chicken_Disease_Classification.config.configuration import ConfigurationManager
+from Chicken_Disease_Classification.components.data_ingestion import DataIngestion
+from Chicken_Disease_Classification import logger
+
+STAGE_NAME = "Data Ingestion Stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file() 
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f"Running stage {STAGE_NAME} started ...")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f"{STAGE_NAME} completed!\n\nx ========= x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
