74006dd5d748a5bf957ac7bf12d10f6c74986bd3
Shubhankargupta691 <shubhankargupta691@gmail.com>
2024-01-02 20:50:47 +0530

    fix: index.html

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ REPO_NAME = "Chicken_Disease_Classification"
 AUTHOR_USER_NAME= "Shubhankargupta691"
 SRC_REPO = "Chicken_Disease_Classification"
 AUTHOR_Email = "shubhankrgupta691@gmail.com"
-CREDIT_USER_NAME= "entbappy"
 
 setuptools.setup(
     name=SRC_REPO ,
@@ -20,7 +19,7 @@ setuptools.setup(
     long_description=long_description, 
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    credit_url=f"https://github.com/{CREDIT_USER_NAME}",
+
     project_urls={
             
     },
